314ff9939b4daffe941c860c19f1f081819565bd
Rithik <jainrithik2000@gmail.com>
2024-01-25 18:31:10 +0530

    Your commit message

---
+++ 1/app.js
@@ -0,0 +1,29 @@
+{
+  /* <div id="parent">
+  <div id="child1">
+    <h1>I am h1</h1>
+    <h2>I am h2</h2>
+  </div>
+  <div id="child2">
+    <h1>I am h1</h1>
+    <h2>I am h2</h2>
+  </div>
+</div>; */
+}
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am H1"),
+    React.createElement("h2", {}, "I am H2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am H1"),
+    React.createElement("h2", {}, "I am H2"),
+  ]),
+]);
+
+console.log(parent); //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent); //convert react elm to html elm
